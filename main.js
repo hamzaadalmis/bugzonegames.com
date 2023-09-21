a0b3c808238e2f6f07b2dd0389863aec35c387b4
Hamza Adalmış <hamzadalmis344@gmail.com>
2023-09-21 20:46:04 +0300

    fix: dark mode bug because of typo, img element renamed to logo, logo will change according to theme

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const icon = document.getElementById('icon');
+const logo = document.getElementById('logo');
 
 icon.onclick = () => {
-    document.body.classList.toggle('dark-theme');
+    document.body.classList.toggle('light-mode');
 
-    if (document.body.classList.contains('dark-theme')) {
-        icon.src = "img/moon.png"
+    if (document.body.classList.contains('light-mode')) {
+        icon.src = "img/moon.png";
+        logo.src = "img/bugzonegames_logo.png";
+
     }
     else {
-        icon.src = "img/sun.png"
+        icon.src = "img/sun.png";
+        logo.src = "img/bugzonegames_white.png"
     }
 }
