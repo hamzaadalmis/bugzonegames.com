e6e04c0235585a967cc44c0a6b5a1ba5fb130c41
Hamza Adalmış <hamzadalmis344@gmail.com>
2023-09-21 22:31:35 +0300

    change: refactor code with movingconst arrow function above

--- main.js
+++ main.js
@@ -3,19 +3,6 @@ const logo = document.getElementById('logo');
 
 let lightMode = localStorage.getItem('lightMode');
 
-if (icon != null) {
-    icon.addEventListener('click', () => {
-        lightMode = localStorage.getItem('lightMode');
-
-        if (lightMode !== 'enabled') {
-            enableLightMode();
-        }
-        else {
-            disableLightMode();
-        }
-    });
-}
-
 const enableLightMode = () => {
     document.body.classList.add('light-mode');
     localStorage.setItem('lightMode', 'enabled');
@@ -28,6 +15,19 @@ const disableLightMode = () => {
     localStorage.setItem('lightMode', null);
     icon.src = 'img/sun.png';
     logo.src = 'img/bugzonegames_white.png';
+}
+
+if (icon != null) {
+    icon.addEventListener('click', () => {
+        lightMode = localStorage.getItem('lightMode');
+
+        if (lightMode !== 'enabled') {
+            enableLightMode();
+        }
+        else {
+            disableLightMode();
+        }
+    });
 }
 
 if (lightMode === 'enabled') {
