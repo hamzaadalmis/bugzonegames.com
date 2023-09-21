e63dbb36f3460a462ad812c5b67a25f1a7bf6e63
Hamza Adalmış <hamzadalmis344@gmail.com>
2023-09-21 20:36:39 +0300

    add: dark mode support

--- main.js
+++ main.js
@@ -1,30 +1,12 @@
 const icon = document.getElementById('icon');
 
-let toggle = true;
+icon.onclick = () => {
+    document.body.classList.toggle('dark-theme');
 
-icon.onclick = switchColorScheme(!toggle);
-
-if (window.matchMedia && window.matchMedia('prefers-color-scheme: dark').matches) {
-    switchColorScheme(true);
-}
-
-window.matchMedia('prefers-color-scheme: dark').addEventListener('change', e => {
-    const newColorScheme = e.matches ? 'dark' : 'light';
-    switchColorScheme(newColorScheme);
-});
-
-function switchColorScheme(dark) {
-    if (dark) {
-        document.body.classList.toggle('dark-theme');
-        console.log("it's dark");
+    if (document.body.classList.contains('dark-theme')) {
+        icon.src = "img/moon.png"
     }
     else {
-        document.body.classList.toggle('dark-theme');
-
-        if (document.body.classList.contains('dark-theme')) {
-            icon.src = "img/moon.png"
-        }
-
-        console.log("it's so bright");
+        icon.src = "img/sun.png"
     }
 }
