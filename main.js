8ca8e97d9f06a320ef8bec3a7b89524f579dbe04
Hamza Adalmış <hamzadalmis344@gmail.com>
2023-09-21 20:34:42 +0300

    change: href before rel icon var and element and selector added

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const icon = document.getElementById('icon');
+
+let toggle = true;
+
+icon.onclick = switchColorScheme(!toggle);
 
 if (window.matchMedia && window.matchMedia('prefers-color-scheme: dark').matches) {
     switchColorScheme(true);
@@ -10,9 +15,16 @@ window.matchMedia('prefers-color-scheme: dark').addEventListener('change', e => 
 
 function switchColorScheme(dark) {
     if (dark) {
+        document.body.classList.toggle('dark-theme');
         console.log("it's dark");
     }
     else {
+        document.body.classList.toggle('dark-theme');
+
+        if (document.body.classList.contains('dark-theme')) {
+            icon.src = "img/moon.png"
+        }
+
         console.log("it's so bright");
     }
 }
