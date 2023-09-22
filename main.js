ef4df1cd78a535a2cd18c183fa631ba81250f10d
Hamza Adalmış <hamzadalmis344@gmail.com>
2023-09-22 12:06:16 +0300

    fix: uncaughted reference error

--- main.js
+++ main.js
@@ -6,21 +6,15 @@ let lightMode = localStorage.getItem('lightMode');
 const enableLightMode = () => {
     document.body.classList.add('light-mode');
     localStorage.setItem('lightMode', 'enabled');
-
-    if (src !== null && logo !== null) {
-        icon.src = 'img/moon.webp';
-        logo.src = 'img/bugzonegames_logo.webp';
-    }
+    icon.src = 'img/moon.webp';
+    logo.src = 'img/bugzonegames_logo.webp';
 };
 
 const disableLightMode = () => {
     document.body.classList.remove('light-mode');
     localStorage.setItem('lightMode', null);
-
-    if (icon !== null && logo !== null) {
-        icon.src = 'img/sun.webp';
-        logo.src = 'img/bugzonegames_white.webp';
-    }
+    icon.src = 'img/sun.webp';
+    logo.src = 'img/bugzonegames_white.webp';
 };
 
 if (icon !== null) {
